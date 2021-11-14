16e0aaafd52052e07b57db0f611ea32e0e445ba5
Saudre Cosaat <alejandro.ruedas87@gmail.com>
2021-11-14 22:47:45 +1300

    Infinite loop visualization

--- game-of-life.r
+++ game-of-life.r
@@ -3,20 +3,25 @@ board_col <- 100
 
 # Initial Game
 
-get_game <- function () {
+get_game <- function() {
     game <- sample(c(1, 0), board_row * board_col, replace = TRUE)
     matrix_game <- matrix(data = game, nrow = board_row, ncol = board_col)
     return(matrix_game)
 }
 
-simulation <- function () {
+simulation <- function() {
     game <- get_game()
     my_colors <- c(1, 5)
     windows(title = "Game of Life")
     frame()
-    par(mar = c(0, 0, 0, 0))
-    image(game, col = my_colors)
-    grid(nx = board_row, ny = board_col, col = grey(0.3), lty = 1)
+
+    # Infinite loop
+
+    while (dev.interactive()) {
+        par(mar = c(0, 0, 0, 0), new = TRUE)
+        image(game, col = my_colors)
+        grid(nx = board_row, ny = board_col, col = grey(0.3), lty = 1)
+    }
 }
 
 simulation()
