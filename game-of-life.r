c9987ddd9aa8478f9a32d1dcd6f96fb4c74e8fca
Saudre Cosaat <alejandro.ruedas87@gmail.com>
2021-11-17 21:55:55 +1300

    bottom left corner addded

--- game-of-life.r
+++ game-of-life.r
@@ -136,6 +136,20 @@ update_game <- function(board) {
         new_board[row_number, j] <- evaluate_rules(board[row_number, j], live_neighbors)
     }
 
+    # left-bottom-corner
+
+    live_neighbors <- sum(
+        board[(row_number - 1), col_number],
+        board[(row_number - 1), 1],
+        board[(row_number - 1), 2],
+        board[row_number, col_number],
+        board[row_number, 2],
+        board[1, col_number],
+        board[1, 1],
+        board[1, 2]
+    )
+    new_board[row_number, 1] <- evaluate_rules(board[row_number, 1], live_neighbors)
+
     return(new_board)
 }
 
